3060bbebff73c2e3e72b06e80f83723c28dfb61d
C-Mierez <carlos.mierez20@gmail.com>
2024-02-08 17:32:08 -0300

    ✨ New update Hive mutation

--- convex/hive.ts
+++ convex/hive.ts
@@ -26,3 +26,48 @@ export const create = mutation({
     return hive;
   },
 });
+
+export const remove = mutation({
+  args: {
+    id: v.id("hive"),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("Unauthorized");
+    }
+
+    // TODO: Remove favourite hives
+
+    await ctx.db.delete(args.id);
+  },
+});
+
+export const update = mutation({
+  args: {
+    id: v.id("hive"),
+    title: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("Unauthorized");
+    }
+
+    const title = args.title.trim();
+
+    if (!title) {
+      throw new Error("Title is required");
+    }
+
+    if (title.length > 80) {
+      throw new Error("Title is too long");
+    }
+
+    const hive = await ctx.db.patch(args.id, { title });
+
+    return hive;
+  },
+});
