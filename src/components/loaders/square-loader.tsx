c8e583c449888fc467a99919efbebdfd142a2339
C-Mierez <carlos.mierez20@gmail.com>
2024-02-08 16:06:27 -0300

    💄 Added loader

---
+++ src/components/loaders/square-loader.tsx
@@ -0,0 +1,16 @@
+import { cn } from "~/lib/utils";
+import css from "./loaders.module.css";
+
+export default function SquareLoader({
+  size = "md",
+}: {
+  size?: "sm" | "md" | "lg";
+}) {
+  const sizeClass = size === "sm" ? css.sm : size === "md" ? css.md : css.lg;
+
+  return (
+    <div className={cn(css.squareWrapper, sizeClass)}>
+      <div className={css.squareLoader}></div>
+    </div>
+  );
+}
