31b9e3f83c7628e38a6b3be4124c40d7faadc03c
C-Mierez <carlos.mierez20@gmail.com>
2024-02-08 17:34:55 -0300

    ✨ Confirm modal

---
+++ src/components/confirm-modal.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "./ui/alert-dialog";
+
+interface ConfirmModalParams {
+  children: React.ReactNode;
+  title: string;
+  description?: string;
+  disabled?: boolean;
+  onConfirm: () => void;
+  onCancel: () => void;
+}
+
+export default function ConfirmModal({
+  children,
+  title,
+  description,
+  disabled,
+  onConfirm,
+  onCancel,
+}: ConfirmModalParams) {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>{children}</AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>{title}</AlertDialogTitle>
+          <AlertDialogDescription>{description}</AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel onClick={onCancel}>Cancel</AlertDialogCancel>
+          <AlertDialogAction disabled={disabled} onClick={onConfirm}>
+            Confirm
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
