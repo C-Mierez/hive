110159e185f10c57d816b738591e22d59688e45b
C-Mierez <carlos.mierez20@gmail.com>
2024-02-08 17:34:34 -0300

    💄 New auth loading animation

--- src/components/auth/loading.tsx
+++ src/components/auth/loading.tsx
@@ -1,8 +1,10 @@
+import SquareLoader from "../loaders/square-loader";
+
 export default function LoadingAuth() {
   return (
-    <div className="flex min-h-[100lvh] flex-col items-center justify-center">
+    <div className="flex min-h-[100lvh] flex-col items-center justify-center gap-4">
       <h1 className="text-6xl font-black uppercase">Hive</h1>
-      <p>Loading</p>
+      <SquareLoader />
     </div>
   );
 }
