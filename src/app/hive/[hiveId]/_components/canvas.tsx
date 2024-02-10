08227fc9301ed42647aa74695ff0e2db33e43bb4
C-Mierez <carlos.mierez20@gmail.com>
2024-02-10 16:18:23 -0300

    ✨ Canvas mode and Canvas state

--- src/app/hive/[hiveId]/_components/canvas.tsx
+++ src/app/hive/[hiveId]/_components/canvas.tsx
@@ -1,19 +1,37 @@
 "use client";
 
+import { useState } from "react";
 import Info from "./info";
 import Participants from "./participants";
 import Toolbar from "./toolbar";
+import { CanvasMode, CanvasState } from "~/types/canvas";
+import { useCanRedo, useCanUndo, useHistory } from "@/liveblocks.config";
 
 interface CanvasParams {
   hiveId: string;
 }
 
 export default function Canvas({ hiveId }: CanvasParams) {
+  const [canvasState, setCanvasState] = useState<CanvasState>({
+    mode: CanvasMode.None,
+  });
+
+  const history = useHistory();
+  const canUndo = useCanUndo();
+  const canRedo = useCanRedo();
+
   return (
     <main className="relative h-screen w-full touch-none bg-muted">
       <Info hiveId={hiveId} />
       <Participants />
-      <Toolbar />
+      <Toolbar
+        canvasState={canvasState}
+        setCanvasState={setCanvasState}
+        undo={history.undo}
+        redo={history.redo}
+        canUndo={canUndo}
+        canRedo={canRedo}
+      />
     </main>
   );
 }
--- src/app/hive/[hiveId]/_components/toolbar.tsx
+++ src/app/hive/[hiveId]/_components/toolbar.tsx
@@ -8,56 +8,124 @@ import {
   TextIcon,
 } from "@radix-ui/react-icons";
 import ToolButton from "./tool-button";
+import { CanvasMode, CanvasState, LayerType } from "~/types/canvas";
 
-export default function Toolbar() {
+interface ToolbarParams {
+  canvasState: CanvasState;
+  setCanvasState: (newState: CanvasState) => void;
+  undo: () => void;
+  redo: () => void;
+  canUndo: boolean;
+  canRedo: boolean;
+}
+
+export default function Toolbar({
+  canvasState,
+  setCanvasState,
+  undo,
+  redo,
+  canUndo,
+  canRedo,
+}: ToolbarParams) {
   return (
     <div className="absolute left-4 top-1/2 grid -translate-y-1/2 grid-flow-row gap-4">
       <div className="flex flex-col gap-2 rounded-sm border-global_sm bg-white px-2 py-2">
         <ToolButton
           label="Cursor"
-          onClick={() => {}}
+          onClick={() =>
+            setCanvasState({
+              mode: CanvasMode.None,
+            })
+          }
           isDisabled={false}
-          isActive={true}
+          isActive={
+            canvasState.mode === CanvasMode.None ||
+            canvasState.mode === CanvasMode.Translating ||
+            canvasState.mode === CanvasMode.SelectionNet ||
+            canvasState.mode === CanvasMode.Pressing ||
+            canvasState.mode === CanvasMode.Resizing
+          }
         >
           <CursorArrowIcon />
         </ToolButton>
+
         <ToolButton
           label="Pen"
-          onClick={() => {}}
+          onClick={() =>
+            setCanvasState({
+              mode: CanvasMode.Pencil,
+            })
+          }
           isDisabled={false}
-          isActive={false}
+          isActive={canvasState.mode === CanvasMode.Pencil}
         >
           <Pencil1Icon />
         </ToolButton>
+
         <ToolButton
           label="Circle"
-          onClick={() => {}}
+          onClick={() =>
+            setCanvasState({
+              mode: CanvasMode.Inserting,
+              layerType: LayerType.Circle,
+            })
+          }
           isDisabled={false}
-          isActive={false}
+          isActive={
+            canvasState.mode === CanvasMode.Inserting &&
+            canvasState.layerType === LayerType.Circle
+          }
         >
           <CircleIcon />
         </ToolButton>
+
         <ToolButton
-          label="Square"
-          onClick={() => {}}
+          label="Rectangle"
+          onClick={() =>
+            setCanvasState({
+              mode: CanvasMode.Inserting,
+              layerType: LayerType.Rectangle,
+            })
+          }
           isDisabled={false}
-          isActive={false}
+          isActive={
+            canvasState.mode === CanvasMode.Inserting &&
+            canvasState.layerType === LayerType.Rectangle
+          }
         >
           <SquareIcon />
         </ToolButton>
+
         <ToolButton
           label="Note"
-          onClick={() => {}}
+          onClick={() =>
+            setCanvasState({
+              mode: CanvasMode.Inserting,
+              layerType: LayerType.Note,
+            })
+          }
           isDisabled={false}
-          isActive={false}
+          isActive={
+            canvasState.mode === CanvasMode.Inserting &&
+            canvasState.layerType === LayerType.Note
+          }
         >
           <FileIcon />
         </ToolButton>
+
         <ToolButton
           label="Text"
-          onClick={() => {}}
+          onClick={() =>
+            setCanvasState({
+              mode: CanvasMode.Inserting,
+              layerType: LayerType.Text,
+            })
+          }
           isDisabled={false}
-          isActive={false}
+          isActive={
+            canvasState.mode === CanvasMode.Inserting &&
+            canvasState.layerType === LayerType.Text
+          }
         >
           <TextIcon />
         </ToolButton>
@@ -65,16 +133,17 @@ export default function Toolbar() {
       <ul className="flex flex-col gap-2 rounded-sm border-global_sm bg-white px-2 py-2">
         <ToolButton
           label="Redo"
-          onClick={() => {}}
-          isDisabled={true}
+          onClick={redo}
+          isDisabled={!canRedo}
           isActive={false}
         >
           <ResetIcon className="scale-x-[-1]" />
         </ToolButton>
+
         <ToolButton
           label="Undo"
-          onClick={() => {}}
-          isDisabled={true}
+          onClick={undo}
+          isDisabled={!canUndo}
           isActive={false}
         >
           <ResetIcon />
