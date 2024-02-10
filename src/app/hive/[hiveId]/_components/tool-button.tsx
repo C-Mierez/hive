dd78bc8ec57b216486b65fd4ec45822347fb4746
C-Mierez <carlos.mierez20@gmail.com>
2024-02-10 15:36:01 -0300

    🚧 Toolbar layout

---
+++ src/app/hive/[hiveId]/_components/tool-button.tsx
@@ -0,0 +1,32 @@
+import { ExitIcon } from "@radix-ui/react-icons";
+import Hint from "~/components/hint";
+import { Button } from "~/components/ui/button";
+
+interface ToolButtonParams {
+  label: string;
+  children: React.ReactNode;
+  onClick: () => void;
+  isDisabled?: boolean;
+  isActive?: boolean;
+}
+
+export default function ToolButton({
+  label,
+  children,
+  onClick,
+  isDisabled,
+  isActive,
+}: ToolButtonParams) {
+  return (
+    <Hint label={label} side="right">
+      <Button
+        disabled={isDisabled}
+        onClick={onClick}
+        size={"icon"}
+        variant={isActive ? "darkColored" : "secondary"}
+      >
+        {children}
+      </Button>
+    </Hint>
+  );
+}
--- src/app/hive/[hiveId]/_components/toolbar.tsx
+++ src/app/hive/[hiveId]/_components/toolbar.tsx
@@ -1,15 +1,84 @@
+import {
+  CircleIcon,
+  CursorArrowIcon,
+  FileIcon,
+  Pencil1Icon,
+  ResetIcon,
+  SquareIcon,
+  TextIcon,
+} from "@radix-ui/react-icons";
+import ToolButton from "./tool-button";
+
 export default function Toolbar() {
   return (
-    <div className="absolute left-4 top-1/2 flex -translate-y-1/2 flex-col gap-4">
-      <ul className="flex flex-col gap-2 rounded-sm border-global_sm bg-white px-4 py-2">
-        <li>Pencil</li>
-        <li>Pencil</li>
-        <li>Pencil</li>
-        <li>Pencil</li>
-      </ul>
-      <ul className="flex flex-col gap-2 rounded-sm border-global_sm bg-white px-4 py-2">
-        <li>Undo</li>
-        <li>Redo</li>
+    <div className="absolute left-4 top-1/2 grid -translate-y-1/2 grid-flow-row gap-4">
+      <div className="flex flex-col gap-2 rounded-sm border-global_sm bg-white px-2 py-2">
+        <ToolButton
+          label="Cursor"
+          onClick={() => {}}
+          isDisabled={false}
+          isActive={true}
+        >
+          <CursorArrowIcon />
+        </ToolButton>
+        <ToolButton
+          label="Pen"
+          onClick={() => {}}
+          isDisabled={false}
+          isActive={false}
+        >
+          <Pencil1Icon />
+        </ToolButton>
+        <ToolButton
+          label="Circle"
+          onClick={() => {}}
+          isDisabled={false}
+          isActive={false}
+        >
+          <CircleIcon />
+        </ToolButton>
+        <ToolButton
+          label="Square"
+          onClick={() => {}}
+          isDisabled={false}
+          isActive={false}
+        >
+          <SquareIcon />
+        </ToolButton>
+        <ToolButton
+          label="Note"
+          onClick={() => {}}
+          isDisabled={false}
+          isActive={false}
+        >
+          <FileIcon />
+        </ToolButton>
+        <ToolButton
+          label="Text"
+          onClick={() => {}}
+          isDisabled={false}
+          isActive={false}
+        >
+          <TextIcon />
+        </ToolButton>
+      </div>
+      <ul className="flex flex-col gap-2 rounded-sm border-global_sm bg-white px-2 py-2">
+        <ToolButton
+          label="Redo"
+          onClick={() => {}}
+          isDisabled={true}
+          isActive={false}
+        >
+          <ResetIcon className="scale-x-[-1]" />
+        </ToolButton>
+        <ToolButton
+          label="Undo"
+          onClick={() => {}}
+          isDisabled={true}
+          isActive={false}
+        >
+          <ResetIcon />
+        </ToolButton>
       </ul>
     </div>
   );
