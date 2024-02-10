fb37c7356ace2beb04dee6c9b9d181b5b20f8228
C-Mierez <carlos.mierez20@gmail.com>
2024-02-10 15:10:04 -0300

    🧑‍💻 User color calculation

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,20 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+const UserColors = [
+  "#B03030",
+  "#17B890",
+  "#17B890",
+  "#326EB3",
+  "#463F3A",
+  "#FE9148",
+  "#E9D858",
+];
+
+export function connectionToColor(connectionId: number) {
+  return UserColors[connectionId % UserColors.length];
 }
