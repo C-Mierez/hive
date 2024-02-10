c1bb2908e07a87b5db6f60ed40490f798dcb0d74
C-Mierez <carlos.mierez20@gmail.com>
2024-02-10 16:17:53 -0300

    💄 Tweaked button hover effect

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -21,7 +21,7 @@ const buttonVariants = cva(
         outline:
           "border border-global_sm bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
-          "border-global_sm bg-secondary text-secondary-foreground hover:bg-secondary",
+          "border-global_sm bg-secondary text-secondary-foreground hover:bg-accent hover:text-background",
         ghost:
           "hover:bg-accent hover:border-global_sm hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
