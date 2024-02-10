269d1c837ed299bc9df7327031bebbc5778bb897
C-Mierez <carlos.mierez20@gmail.com>
2024-02-10 15:09:45 -0300

    🐛 Changed firstname data for username

--- src/app/api/liveblocks-auth/route.ts
+++ src/app/api/liveblocks-auth/route.ts
@@ -29,7 +29,7 @@ export async function POST(request: Request) {
   }
 
   const userInfo = {
-    name: user.firstName || "Fellow Bee",
+    name: user.username || "Fellow Bee",
     picture: user.imageUrl!,
   };
 
