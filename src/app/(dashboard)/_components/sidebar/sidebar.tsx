29273462b604750b19c87c1f12cc5793f120c98e
C-Mierez <carlos.mierez20@gmail.com>
2024-02-10 12:04:33 -0300

    🚧 TODO

--- src/app/(dashboard)/_components/sidebar/sidebar.tsx
+++ src/app/(dashboard)/_components/sidebar/sidebar.tsx
@@ -5,7 +5,8 @@ import ColonyList from "./colony-list";
 
 export default function Sidebar() {
   return (
-    <aside className="sticky left-0 top-0 z-50  h-screen  border-r-global_sm bg-muted">
+    // TODO: Handle overflow scrolling behaviour
+    <aside className="sticky left-0 top-0 z-50  h-screen border-r-global_sm bg-muted">
       <Link
         href="/"
         className="h-navbar grid w-full place-items-center border-b-global_sm border-background px-4 font-black transition-colors hover:border-foreground hover:bg-primary"
