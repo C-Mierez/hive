03f4ff260d04ac4819115d4da5f1d489a499f301
C-Mierez <carlos.mierez20@gmail.com>
2024-02-10 15:10:21 -0300

    ✨ Participants canvas component

--- src/app/hive/[hiveId]/_components/participants.tsx
+++ src/app/hive/[hiveId]/_components/participants.tsx
@@ -1,7 +1,62 @@
+"use client";
+
+import { useOthers, useSelf } from "@/liveblocks.config";
+import CanvasAvatar from "./user-avatar";
+import { connectionToColor } from "~/lib/utils";
+
+const MAX_SHOWN_COLLABORATORS = 2;
+
 export default function Participants() {
+  const users = useOthers();
+  const currentUser = useSelf();
+  const hasMoreUsers = users.length > MAX_SHOWN_COLLABORATORS;
+
   return (
-    <div className="absolute right-4 top-4 grid place-items-center rounded-sm border-global_sm bg-background px-4 py-2">
-      Participants
+    <div className="absolute right-4 top-4 grid grid-flow-col items-center gap-2">
+      {hasMoreUsers && (
+        <span className="text-md text-muted-foreground">
+          +{users.length - MAX_SHOWN_COLLABORATORS}
+        </span>
+      )}
+      {users.slice(0, MAX_SHOWN_COLLABORATORS).map((user) => (
+        <AvatarWrapper color={connectionToColor(user.connectionId)}>
+          <CanvasAvatar
+            key={user.id}
+            src={user.info?.picture}
+            name={user.info?.name}
+            fallback={user.info?.name?.charAt(0).toUpperCase() || "B"}
+          />
+        </AvatarWrapper>
+      ))}
+      {currentUser && (
+        <AvatarWrapper color={connectionToColor(currentUser.connectionId)}>
+          <CanvasAvatar
+            src={currentUser.info?.picture}
+            name={"You"}
+            fallback={currentUser.info?.name?.charAt(0).toUpperCase() || "B"}
+          />
+        </AvatarWrapper>
+      )}
+    </div>
+  );
+}
+
+function AvatarWrapper({
+  children,
+  color,
+}: {
+  children: React.ReactNode;
+  color?: string;
+}) {
+  return (
+    <div className="flex flex-col gap-1">
+      {children}
+      <div
+        className="h-[0.125rem] w-full rounded-sm"
+        style={{
+          backgroundColor: color,
+        }}
+      ></div>
     </div>
   );
 }
---
+++ src/app/hive/[hiveId]/_components/user-avatar.tsx
@@ -0,0 +1,28 @@
+import Hint from "~/components/hint";
+import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
+import { cn } from "~/lib/utils";
+
+interface CanvasAvatarParams {
+  src?: string;
+  name?: string;
+  fallback?: string;
+  color?: string;
+}
+
+export default function CanvasAvatar({
+  src,
+  name,
+  fallback,
+  color,
+}: CanvasAvatarParams) {
+  return (
+    <Hint label={name || "Fellow Bee"} side="bottom">
+      <Avatar className={cn("h-8 w-8 rounded-sm")}>
+        <AvatarImage src={src} />
+        <AvatarFallback className="text-sm font-medium">
+          {fallback}
+        </AvatarFallback>
+      </Avatar>
+    </Hint>
+  );
+}
